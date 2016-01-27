ce7226b5a936b9ff3a10f2587d3badaaa8458d86
mlmconseil <mlmconseil@debian>
2016-01-27 20:17:43 +0100

    correction de la conversion

--- total_en_lettres/conversion.py
+++ total_en_lettres/conversion.py
@@ -1,80 +1,126 @@
-# -*- coding: utf-8 -*-
-################################################################################
-#                                                                              
-#                        Module [Total en lettres]                         
-#                    ----------------------------------                        
-#                      permet d'ajouter une convertion                         
-#                        du total en lettres (Devis et Facture)                           
-#                    ----------------------------------                        
-#                                                                              
-#                       langage : Python 2.7                                   
-#                       date creation : 01/01/2016                            
-#                       date modification : 04/01/2016                         
-#                       version : 0.1                                          
-#                       auteur  : MLMConseil                              
-#                                                                              
-################################################################################
-schu=["","UN ","DEUX ","TROIS ","QUATRE ","CINQ ","SIX ","SEPT ","HUIT ","NEUF "]
-schud=["DIX ","ONZE ","DOUZE ","TREIZE ","QUATORZE ","QUINZE ","SEIZE ","DIX SEPT ","DIX HUIT ","DIX NEUF "]
-schd=["","DIX ","VINGT ","TRENTE ","QUARANTE ","CINQUANTE ","SOIXANTE ","SOIXANTE ","QUATRE VINGT ","QUATRE VINGT "]
-def Nombre2lettres(nombre):
-    s=''
-    reste=nombre
-    i=1000000000 
-    while i>0:
-        y=reste/i
-        if y!=0:
-            centaine=y/100
-            dizaine=(y - centaine*100)/10
-            unite=y-centaine*100-dizaine*10
-            if centaine==1:
-                s+="CENT "
-            elif centaine!=0:
-                s+=schu[centaine]+"CENT "
-                if dizaine==0 and unite==0: s=s[:-1]+"S " 
-            if dizaine not in [0,1]: s+=schd[dizaine] 
-            if unite==0:
-                if dizaine in [1,7,9]: s+="DIX "
-                elif dizaine==8: s=s[:-1]+"S "
-            elif unite==1:   
-                if dizaine in [1,9]: s+="ONZE "
-                elif dizaine==7: s+="ET ONZE "
-                elif dizaine in [2,3,4,5,6]: s+="ET UN "
-                elif dizaine in [0,8]: s+="UN "
-            elif unite in [2,3,4,5,6,7,8,9]: 
-                if dizaine in [1,7,9]: s+=schud[unite] 
-                else: s+=schu[unite] 
-            if i==1000000000:
-                if y>1: s+="MILLIARDS "
-                else: s+="MILLIARD "
-            if i==1000000:
-                if y>1: s+="MILLIONS "
-                else: s+="MILLIONS "
-            if i==1000:
-                s+="MILLE "
-        reste -= y*i
-        dix=False
-        i/=1000;
-    if len(s)==0: s+="ZERO "
-    return s
+# -*- encoding: utf-8 -*-
 
+"""
+Traduction d'un nombre en texte.
+Realisation : Michel Claveau    http://mclaveau.com
+
+SVP, n'enlevez pas mon adresse/URL ; merci d avance
+
+Usage : voir les exemples, la fin du script.
+
+Note : traduction franco-francaise, avec unites variables, orthographe gere, unites et centiemes.
+"""
+
+
+def tradd(num):
+    global t1,t2
+    ch=''
+    if num==0 :
+        ch=''
+    elif num<20:
+        ch=t1[num]
+    elif num>=20:
+        if (num>=70 and num<=79)or(num>=90):
+            z=int(num/10)-1
+        else:
+            z=int(num/10)
+        ch=t2[z]
+        num=num-z*10
+        if (num==1 or num==11) and z<8:
+            ch=ch+' et'
+        if num>0:
+            ch=ch+' '+tradd(num)
+        else:
+            ch=ch+tradd(num)
+    return ch
+
+
+def tradn(num):
+    global t1,t2
+    ch=''
+    flagcent=False
+    if num>=1000000000:
+        z=int(num/1000000000)
+        ch=ch+tradn(z)+' milliard'
+        if z>1:
+            ch=ch+'s'
+        num=num-z*1000000000
+    if num>=1000000:
+        z=int(num/1000000)
+        ch=ch+tradn(z)+' million'
+        if z>1:
+            ch=ch+'s'
+        num=num-z*1000000
+    if num>=1000:
+        if num>=100000:
+            z=int(num/100000)
+            if z>1:
+                ch=ch+' '+tradd(z)
+            ch=ch+' cent'
+            flagcent=True
+            num=num-z*100000
+            if int(num/1000)==0 and z>1:
+                ch=ch+'s'
+        if num>=1000:
+            z=int(num/1000)
+            if (z==1 and flagcent) or z>1:
+                ch=ch+' '+tradd(z)
+            num=num-z*1000
+        ch=ch+' mille'
+    if num>=100:
+        z=int(num/100)
+        if z>1:
+            ch=ch+' '+tradd(z)
+        ch=ch+" cent"
+        num=num-z*100
+        if num==0 and z>1:
+           ch=ch+'s'
+    if num>0:
+        ch=ch+" "+tradd(num)
+    return ch
+
+
+def trad(nb, unite="dinar", decim="centime"):
+    global t1,t2
+    nb=round(nb,2)
+    t1=["","un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze","treize","quatorze","quinze","seize","dix-sept","dix-huit","dix-neuf"]
+    t2=["","dix","vingt","trente","quarante","cinquante","soixante","soixante-dix","quatre-vingt","quatre-vingt dix"]
+    z1=int(nb)
+    z3=(nb-z1)*100
+    z2=int(round(z3,0))
+    if z1==0:
+        ch=u"zéro"
+    else:
+        ch=tradn(abs(z1))
+    if z1>1 or z1<-1:
+        if unite!='':
+            ch=ch+" "+unite+'s'
+    else:
+        ch=ch+" "+unite
+    if z2>0:
+#        ch=ch+tradn(z2)
+        ch=ch+" et"+tradn(z2) # ajout de 'et'  cinq dinars et vingt centimes
+        if z2>1 or z2<-1:
+            if decim!='':
+                ch=ch+" "+decim+'s'
+        else:
+            ch=ch+" "+decim
+    if nb<0:
+        ch="moins "+ch
+    return ch
+
+
+
+# if __name__=='__main__':
+
+#     print 183.93,trad(183.93)
 
 def convertir_fac(total):
     """conversion de facture"""
-    total = str(total)
-    total = total.replace('.', ',')
-    nbr=total.split(',') 
-    lettre1 = Nombre2lettres(int(nbr[0]))
-    lettre2 = Nombre2lettres(int(nbr[1]))
-    lettres = "\nArrêtée la presente facture à la somme de "+str(lettre1)+"DINARS" + " ET "+ str(lettre2)+ "CENTIME.".encode('utf8')
-    return lettres
+    total_lettres = "\nArrêtée la presente facture à la somme de "+trad(total).encode('utf8')
+    return total_lettres
 
 def convertir_dev(total):
-    """conversion du devis"""   
-    total = str(total)
-    total = total.replace('.', ',')
-    nbr=total.split(',') 
-    lettre1 = Nombre2lettres(int(nbr[0]))
-    lettre2 = Nombre2lettres(int(nbr[1]))
-    lettres = "\nArrêté le present devis à la somme de "+str(lettre1)+"DINARS" + " ET "+ str(lettre2)+ "CENTIME.".encode('utf8')
-    return lettres
+    """conversion du devis"""
+    total_lettres = "\nArrêté le present devis à la somme de "+trad(total).encode('utf8')
+    return total_lettres
